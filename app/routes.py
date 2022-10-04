f6f23f154f2727b2fe11b1439035493cd3db1d1c
Justinding765 <56009508+Justinding765@users.noreply.github.com>
2022-10-04 10:29:22 -0400

    updated app folder

---
+++ app/routes.py
@@ -0,0 +1,48 @@
+
+from flask import request
+from app import app
+from app.db import get_db
+import sqlite3 
+import sqlite3 as sql
+from flask import g
+
+def get_db():
+    db = getattr(g, '_database', None)
+    #creates database
+    if db is None:
+        db = g._database = sqlite3.connect("database.db")
+    return db
+@app.route('/')
+def route():
+    return 'Hi!'
+@app.route('/example', methods=['GET', 'POST'])
+def example():
+    db = get_db()
+    if request.method == 'POST':
+        db.execute('INSERT INTO Example (contents) VALUES (?)',
+            (request.form['message'],))
+        db.commit()
+    messages = db.execute('SELECT * FROM Example').fetchall()
+    return {'messages': list(map(dict, messages))}
+
+@app.route('/Create_Group', methods=['POST', 'GET'])
+def Create_Group():
+    db = get_db()
+    mycursor = db.cursor()
+    if request.method == 'POST':
+        try:
+            db.execute("CREATE TABLE Groups (id INTEGER PRIMARY KEY AUTOINCREMENT, Size INTEGER, Username TEXT, User_id TEXT)")
+            mycursor.execute('''
+                INSERT INTO Groups ( Size, Username, User_id )
+                VALUES
+                ( 1, "dingjust", "2")
+                ''')
+            db.commit()
+        except Exception:
+            print("Error")
+        
+        
+    return {'messages': [request.method]}
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
