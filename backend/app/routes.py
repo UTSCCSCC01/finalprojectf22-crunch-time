5281d67a21242a0cd9769d42bab943d80e9bb7cb
Justin Ding <justinding7575@gmail.com>
2022-10-06 19:52:34 -0400

    updated feature branch

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,14 @@
-from flask import request
 
+from flask import request
 from app import app
 from app.db import get_db
+import sqlite3 
+import sqlite3 as sql
+from flask import g
+
+@app.route('/')
+def route():
+    return 'Hi!'
 
 @app.route('/example', methods=['GET', 'POST'])
 def example():
@@ -12,4 +19,21 @@ def example():
         db.commit()
     messages = db.execute('SELECT * FROM Example').fetchall()
     return {'messages': list(map(dict, messages))}
-d
+
+@app.route('/Create_Group', methods=['POST', 'GET'])
+def Create_Group():
+    db = get_db()
+    if request.method == 'POST':
+        db.execute("CREATE TABLE Groups (id INTEGER PRIMARY KEY AUTOINCREMENT, Size INTEGER, Username TEXT, User_id TEXT)")
+        db.execute('''
+            INSERT INTO Groups ( Size, Username, User_id )
+            VALUES
+            ( 1, "ding", "2")
+            ''')
+        db.commit()
+    
+        
+    return {'messages': [request.method]}
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
