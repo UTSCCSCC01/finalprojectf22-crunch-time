7e6d2a4f6a19373aa8816aa24eefe3e62e8208b5
IangolaAndri <113553352+IangolaAndri@users.noreply.github.com>
2022-10-06 18:25:50 -0400

    add user class with login logout methods + routes
    add user class with login logout methods and login logout routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,8 @@
-from flask import request
+from flask import request, redirect 
 
 from app import app
 from app.db import get_db
+from app.user import User
 
 @app.route('/example', methods=['GET', 'POST'])
 def example():
@@ -12,3 +13,18 @@ def example():
         db.commit()
     messages = db.execute('SELECT * FROM Example').fetchall()
     return {'messages': list(map(dict, messages))}
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    db = get_db()
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    User.loginUser(email, password, db, db)
+    return redirect('/')
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    db = get_db()
+    User.logoutUser(db)
+    return redirect('/')
