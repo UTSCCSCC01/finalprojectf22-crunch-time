1d55f4268bc2bfe3a0d71cf24fa40c9fbd05f328
Umenemo <37584435+Umenemo@users.noreply.github.com>
2022-10-06 23:35:19 -0400

    Search function works for demo purposes
    Still need to refactor it for the group schema

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -16,8 +16,7 @@ def example():
 def search():
     db = get_db()
     if request.method == 'POST':
-        messages = db.execute("SELECT * FROM Example WHERE contents like '%?%'",(request.form['message'],)).fetchall()
-        #return {'message': 'SELECT * FROM Example WHERE contents like \'%?%\''.format((request.form['message'],))}
+        messages = db.execute("SELECT * FROM Example WHERE contents like ?", ["%" + request.form['message'] + "%"] ).fetchall()
     else:
         messages = db.execute('SELECT * FROM Example').fetchall()
     return {'messages': list(map(dict, messages))}
