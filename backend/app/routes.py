6d961d7516b9835f6bab260a35ee47d60def8a5e
Justinding765 <justin.ding@mail.utoronto.ca>
2022-10-16 19:43:43 -0400

    implemented chat feature using react

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -6,9 +6,7 @@ import sqlite3
 import sqlite3 as sql
 from flask import g
 from app.user import User
-from flask_socketio import SocketIO, join_room, leave_room, emit
-from flask_session import Session
-
+from flask_socketio import SocketIO, send
 """@app.route('/')
 def route():
     return 'Hi!'"""
@@ -70,56 +68,19 @@ def Create_Group():
     
         
     return {'messages': [request.method]}
+app.config['SECRET_KEY'] = 'mysecret'
+
+socketIo = SocketIO(app, cors_allowed_origins="*")
 
 app.debug = True
-app.config['SECRET_KEY'] = 'secret'
-app.config['SESSION_TYPE'] = 'filesystem'
-
-Session(app)
-
-socketio = SocketIO(app, manage_session=False)
+app.host = 'localhost'
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
-
-@app.route('/chat', methods=['GET', 'POST'])
-def chat():
-    if(request.method=='POST'):
-        username = request.form['username']
-        room = request.form['room']
-        #Store the data in session
-        session['username'] = username
-        session['room'] = room
-        return render_template('chat.html', session = session)
-    else:
-        if(session.get('username') is not None):
-            return render_template('chat.html', session = session)
-        else:
-            return redirect(url_for('index'))
-
-@socketio.on('join', namespace='/chat')
-def join(message):
-    room = session.get('room')
-    join_room(room)
-    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
-
-
-@socketio.on('text', namespace='/chat')
-def text(message):
-    room = session.get('room')
-    emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
-
-
-@socketio.on('left', namespace='/chat')
-def left(message):
-    room = session.get('room')
-    username = session.get('username')
-    leave_room(room)
-    session.clear()
-    emit('status', {'msg': username + ' has left the room.'}, room=room)
-
+@socketIo.on("message")
+def handleMessage(msg):
+    print(msg)
+    send(msg, broadcast=True)
+    return None
 
 
 if __name__ == '__main__':
--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,51 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import io from "socket.io-client";
+
 let endPoint = "http://localhost:5000";
-let socket = io.connect(`${endPoint}`); 
+let socket = io.connect(`${endPoint}`);
 
-const Chat = () => {
-  const [messages, setMessages] = useState(["Hello And Welcome"]);
-  const [message, setMessage] = useState("");
+class Chat extends Component {
+  state = {
+    messages: ["Hello and Welcome"],
+    message: ""
+  };
 
-  useEffect(() => {
-    getMessages();
-  }, [messages.length]);
-
-  const getMessages = () => {
+  componentDidMount = () => {
     socket.on("message", msg => {
-      //   let allMessages = messages;
-      //   allMessages.push(msg);
-      //   setMessages(allMessages);
-      setMessages([...messages, msg]);
+      this.setState({
+        messages: [...this.state.messages, msg]
+      });
     });
   };
 
-  // On Change
-  const onChange = e => {
-    setMessage(e.target.value);
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   };
 
-  // On Click
-  const onClick = () => {
+  onClick = () => {
+    const { message } = this.state;
     if (message !== "") {
+      this.setState({
+        message: ""
+      });
       socket.emit("message", message);
-      setMessage("");
     } else {
       alert("Please Add A Message");
     }
   };
 
-  return (
-    <div>
-      {messages.length > 0 &&
-        messages.map(msg => (
-          <div>
-            <p>{msg}</p>
-          </div>
-        ))}
-      <input value={message} name="message" onChange={e => onChange(e)} />
-      <button onClick={() => onClick()}>Send Message</button>
-    </div>
-  );
-};
+  render() {
+    const { messages, message } = this.state;
+    return (
+      <div>
+        {messages.length > 0 &&
+          messages.map(msg => (
+            <div>
+              <p>{msg}</p>
+            </div>
+          ))}
+        <input
+          value={message}
+          name="message"
+          onChange={e => this.onChange(e)}
+        />
+        <button onClick={() => this.onClick()}>Send Message</button>
+      </div>
+    );
+  }
+}
 export default Chat
