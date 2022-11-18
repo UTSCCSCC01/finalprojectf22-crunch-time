79227d2e4695ae2c3b44d22ac02c0354e512ce5b
shaahid <94465319+shaahid62@users.noreply.github.com>
2022-11-18 12:22:57 -0500

    20-kick-user-from-group
    Kick User full functionality complete

--- frontend/src/components/viewGroup.jsx
+++ frontend/src/components/viewGroup.jsx
@@ -41,7 +41,6 @@ function ViewGroup(props) {
     alert("Add friend")
       window.location.reload()
    } 
-
    function kickUser(e, user_id, group_id){
     e.preventDefault();
     fetch("/kick_user/" + user_id + '/' + group_id,{
@@ -55,7 +54,6 @@ function ViewGroup(props) {
     alert("Kick User?")
       window.location.reload()
    } 
-  
   useEffect(fetchInfo, []);
 
   return (
@@ -69,8 +67,8 @@ function ViewGroup(props) {
             <li key={user.user_id}>
               {user.firstName + ' ' + user.lastName}
               <button onClick={(e)=> addfriend(e, user.user_id)}>Add friend</button>
-              <button onClick={(e)=> kickUser(e, user.user_id, groupID)}>Kick User</button>             
-            </li> 
+              <button onClick={(e)=> kickUser(e, user.user_id, groupID)}>Kick User</button>
+            </li>
           ))}
         </ul>
         <h2>Actions</h2>
